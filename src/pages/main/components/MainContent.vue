5cc78d067af28599be9dc12c0d05f4d502837033
Adrian Rudnicki <adi2602@wp.pl>
2023-03-20 21:57:16 +0100

    Navigation drawer in Main component

---
+++ src/pages/main/components/MainContent.vue
@@ -0,0 +1,66 @@
+<template>
+  <CFlex align="center" justify="center" w="100%" minH="80vh">
+    <CBox
+      maxW="400px"
+      w="100%"
+      h="70vh"
+      rounded="1rem"
+      bg="gray.800"
+      :bgImage="require('@/assets/graphics/test.png')"
+      bgPos="top"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <CFlex
+        direction="column"
+        rounded="1rem"
+        h="100%"
+        bgGradient="linear(to-b, whiteAlpha.50 60%, blackAlpha.900 100%)"
+        justify="space-between"
+      >
+        <CBox></CBox>
+        <CStack p="1rem">
+          <CHeading size="md"
+            >{ profileName }, { profileAge }
+            <CText fontSize="xs">{ profileCity } </CText></CHeading
+          >
+
+          <CText
+            fontSize="xs"
+            lineHeight="1.2rem"
+            :maxH="showMore ? '' : '2.4rem'"
+            :overflow="showMore ? '' : 'hidden'"
+          >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+            obcaecati quibusdam impedit, dolore nostrum possimus! Non quam
+            reiciendis unde, labore blanditiis tenetur, perferendis modi
+            impedit, soluta error officiis iusto cupiditate.
+          </CText>
+          <CButton size="sm" @click="showMore = !showMore">{{
+            showMore ? "mniej" : "wiecej"
+          }}</CButton>
+        </CStack>
+      </CFlex>
+    </CBox>
+  </CFlex>
+</template>
+
+<script>
+import { CFlex, CBox, CStack, CText, CButton } from "@chakra-ui/vue";
+export default {
+  components: {
+    CFlex,
+    CText,
+    CBox,
+    CStack,
+    CButton,
+  },
+  data() {
+    return {
+      showMore: false,
+    };
+  },
+};
+</script>
+
+<style></style>
