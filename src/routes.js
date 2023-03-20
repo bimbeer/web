7c07e88907e09cd1f98789bf2829ba23a35c5de4
Adrian Rudnicki <adi2602@wp.pl>
2023-03-20 21:57:59 +0100

    Routes update

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import Welcome from "@/pages/welcome/Welcome.vue";
 import Login from "@/pages/login/Login.vue";
+import Main from "@/pages/main/Main.vue";
+import Pair from "@/pages/pair/Pair.vue";
+import Message from "@/pages/message/Message.vue";
+import Profile from "@/pages/profile/Profile.vue";
 
 export default [
   { path: "/", component: Welcome },
   { path: "/login", component: Login },
+  {
+    path: "/main",
+    component: Main,
+    children: [
+      { path: "pair", component: Pair },
+      { path: "message", component: Message },
+      { path: "profile", component: Profile },
+    ],
+  },
 ];
