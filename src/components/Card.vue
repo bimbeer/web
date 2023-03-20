e4829f1312a0bb4dc57cdbfaca193a65d21cd028
Adrian Rudnicki <adi2602@wp.pl>
2023-03-20 13:32:39 +0100

    Create reusabe card for components

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <CFlex justify="center" align="center" w="100%">
+    <CBox maxW="400px" w="100%">
+      <CStack p="2rem" borderRadius="1rem" bg="gray.800">
+        <CHeading size="lg" pb="2rem" textAlign="center">
+          <slot name="heading"></slot>
+        </CHeading>
+        <CBox>
+          <slot name="body"></slot>
+        </CBox>
+      </CStack>
+    </CBox>
+  </CFlex>
+</template>
+
+<script>
+import { CBox, CFlex, CHeading, CStack } from "@chakra-ui/vue";
+export default {
+  name: "Card",
+  components: {
+    CBox,
+    CFlex,
+    CHeading,
+    CStack,
+  },
+};
+</script>
+
+<style></style>
