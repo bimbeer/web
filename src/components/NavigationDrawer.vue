cc29231aa7ccfd2c6c96f744f8279f513a2682e1
Adrian Rudnicki <adi2602@wp.pl>
2023-03-22 10:36:38 +0100

    Header with NavigationDrawer for global use

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <CBox p="1rem" ariaLabel="main-drawer" maxH="20vh">
+      <CIconButton
+        icon="bars"
+        @click="isOpen = true"
+        bg="none"
+        size="lg"
+        color="yellow.200"
+        ariaLabel="menu"
+      />
+    </CBox>
+
+    <CDrawer :isOpen="isOpen" placement="left" :on-close="close">
+      <CDrawerOverlay />
+      <CDrawerContent :maxW="{ sm: '100%', md: '50%', lg: '30%' }">
+        <CDrawerHeader p="1rem">
+          <CFlex justify="left">
+            <CIconButton
+              icon="xmark"
+              ariaLabel="close"
+              @click="isOpen = false"
+              mb="1rem"
+              bg="none"
+              color="yellow.200"
+              size="lg"
+            />
+          </CFlex>
+          <CFlex justify="space-between" align="center" direction="row">
+            <CText> {nickname} </CText>
+            <CButton
+              size="sm"
+              variant-color="yellow"
+              variant="outline"
+              rounded="1rem"
+            >
+              Wyloguj
+            </CButton>
+          </CFlex>
+        </CDrawerHeader>
+
+        <CDrawerBody p="1rem">
+          <CList>
+            <CListItem v-for="link in links" :key="link.to">
+              <router-link :to="link.to">
+                <CBox
+                  :color="pathname === `${link.to}` ? 'gray.900' : ''"
+                  :bg="pathname === `${link.to}` ? 'yellow.200' : ''"
+                  :_hover="{
+                    bg: 'yellow.100',
+                    ml: '1rem',
+                    color: 'gray.900',
+                  }"
+                  transition="all 0.15s ease-in"
+                  p="1rem"
+                  rounded="1rem"
+                  mb="0.25rem"
+                >
+                  {{ link.label }}
+                </CBox>
+              </router-link>
+            </CListItem>
+          </CList>
+        </CDrawerBody>
+
+        <CDrawerFooter> </CDrawerFooter>
+      </CDrawerContent>
+    </CDrawer>
+  </div>
+</template>
+
+<script>
+import {
+  CButton,
+  CDarkMode,
+  CIconButton,
+  CList,
+  CListItem,
+  CListIcon,
+  CBox,
+  CDrawer,
+  CDrawerOverlay,
+  CDrawerCloseButton,
+  CDrawerBody,
+  CDrawerContent,
+  CDrawerFooter,
+  CDrawerHeader,
+  CFlex,
+  CText,
+} from "@chakra-ui/vue";
+
+export default {
+  name: "NavigationDrawer",
+  components: {
+    CDarkMode,
+    CIconButton,
+    CFlex,
+    CText,
+    CButton,
+    CList,
+    CListItem,
+    CListIcon,
+    CBox,
+    CDrawer,
+    CDrawerOverlay,
+    CDrawerCloseButton,
+    CDrawerBody,
+    CDrawerContent,
+    CDrawerFooter,
+    CDrawerHeader,
+  },
+  data() {
+    return {
+      pathname: this.$route.path,
+      isOpen: false,
+      links: [
+        {
+          to: "/main",
+          label: "Strona główna",
+        },
+        {
+          to: "/main/pair",
+          label: "Pary",
+        },
+        {
+          to: "/main/message",
+          label: "Wiadomości",
+        },
+        {
+          to: "/main/profile",
+          label: "Mój profil",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    close() {
+      this.isOpen = false;
+    },
+  },
+
+  watch: {
+    "$route.path": function (newPath, oldPath) {
+      this.pathname = newPath;
+    },
+  },
+};
+</script>
+
+<style></style>
