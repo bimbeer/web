1f6bc5caea23a167826f6f3b12fcd1d682b69c05
Adrian Rudnicki <adi2602@wp.pl>
2023-03-22 21:20:18 +0100

    add route to register page

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import Pair from "@/pages/pair/Pair.vue";
 import Message from "@/pages/message/Message.vue";
 import Profile from "@/pages/profile/Profile.vue";
 import Recs from "@/pages/recs/Recs.vue";
+import Register from "@/pages/register/Register.vue";
 
 export default [
   { path: "/", component: Welcome },
   { path: "/login", component: Login },
+  { path: "/register", component: Register },
   {
     path: "/main",
     component: Main,
