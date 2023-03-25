c80efaeb546cdb52354faa515593681a0a46e43c
Adrian Rudnicki <adi2602@wp.pl>
2023-03-25 02:25:18 +0100

    almost new CardProfile with animation

--- src/components/CardProfile.vue
+++ src/components/CardProfile.vue
@@ -1,80 +1,92 @@
 <template>
-  <CBox w="100%" h="75vh">
-    <CFlex w="100%" h="100%" align="center" justify="center">
-      <CBox
-        :w="{
-          base: `${(70 / 4) * 3}vh`,
-        }"
-        :h="{
-          base: `${70}vh`,
-        }"
-      >
-        <CBox
-          w="100%"
-          h="100%"
-          bg="black"
-          rounded="1rem"
-          overflow="hidden"
-          :bgImage="require('@/assets/graphics/test.png')"
-          bgPos="top"
-          bgRepeat="no-repeat"
-          bgSize="cover"
-        >
-          <CFlex h="100%" w="100%" direction="column">
-            <CFlex w="100%" p="1rem" gap="2" h="10%">
-              <CBox
-                maxH="4px"
-                borderWidth="2px"
-                rounded="10rem"
-                borderColor="whiteAlpha.600"
-                w="100%"
-              />
-              <CBox
-                maxH="6px"
-                borderWidth="3px"
-                rounded="10rem"
-                borderColor="whiteAlpha.800"
-                w="100%"
-              />
-              <CBox
-                maxH="4px"
-                borderWidth="2px"
-                rounded="10rem"
-                borderColor="whiteAlpha.700"
-                w="100%"
-              />
-            </CFlex>
-            <CFlex justify="space-between" align="center" h="65%">
-              <CIconButton size="lg" bg="none" icon="chevron-left" />
-              <CIconButton size="lg" bg="none" icon="chevron-right" />
-            </CFlex>
-
-            <CFlex
-              p="1rem"
-              pt="3rem"
-              h="25%"
-              bgGradient="linear(to-b, #00000000 0%, blackAlpha.900 70%)"
-              direction="column"
-              overflow="hidden"
+  <CBox maxW="450px" w="100%" h="600px">
+    <CBox
+      bg="black"
+      rounded="1rem"
+      overflow="hidden"
+      :bgImage="require('@/assets/graphics/test.png')"
+      bgPos="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      h="100%"
+    >
+      <CFlex w="100%" h="100%" direction="column" position="relative">
+        <CBox flex="1" p="0.5rem" px="1rem">
+          <CFlex gap="2" justify="center" align="center" h="10px">
+            <CBox
+              v-for="photo in photos"
+              :key="photo"
+              :class="
+                showPhotoNumber === photo ? 'animationShow' : 'animationNotShow'
+              "
+              :bg="showPhotoNumber === photo ? 'yellow.200' : 'blackAlpha.400'"
+              :h="showPhotoNumber === photo ? '8px' : '4px'"
+              w="100%"
+              rounded="50rem"
             >
-              <CFlex direction="column" gap="1rem">
-                <CHeading size="md">{ profileName }, { profileAge }</CHeading>
-                <CText font-size="sm">
-                  { profileDesc } Lorem, ipsum dolor sit amet consectetur
-                  adipisicing elit. Velit natus fugiat rerum error, deleniti
-                  laborum nemo aperiam odio incidunt temporibus repellendus nam
-                  pariatur aliquid nisi maxime blanditiis explicabo animi
-                  voluptate. Lorem ipsum dolor sit amet consectetur adipisicing
-                  elit. Eum nulla voluptatum dolorem quas quidem voluptate,
-                  cupiditate repellendus ad sint, vero velit consequatur magnam
-                  perferendis ducimus dolores inventore, repellat a similique.
-                </CText>
-              </CFlex>
-            </CFlex>
+              <div class="animation"></div>
+            </CBox>
           </CFlex>
         </CBox>
-      </CBox>
-    </CFlex>
+        <CBox flex="1">
+          <CFlex
+            h="100%"
+            justify="space-between"
+            align="center"
+            color="blackAlpha.400"
+          >
+            <CIconButton
+              size="lg"
+              icon="chevron-left"
+              bg=""
+              :_hover="{ transform: 'scale(1.3)', color: 'blackAlpha.800' }"
+              @click="prevPhoto()"
+              ariaLabel="prev-photo"
+            />
+            <CIconButton
+              size="lg"
+              icon="chevron-right"
+              bg=""
+              :_hover="{ transform: 'scale(1.3)', color: 'blackAlpha.800' }"
+              @click="nextPhoto()"
+              ariaLabel="next-photo"
+            />
+          </CFlex>
+        </CBox>
+        <CBox flex="1" />
+        <CBox
+          position="absolute"
+          bottom="0"
+          bgGradient="linear(to-b, #ffffff00 0%, black 70%)"
+          w="100%"
+        >
+          <CFlex p="1rem" py="2rem" justify="space-between">
+            <CBox>
+              <CHeading size="lg">{userName}, {userAge}</CHeading>
+              <CText fontSize="sm"> {userCity} </CText>
+            </CBox>
+            <CIconButton
+              icon="circle-info"
+              size="lg"
+              bg=""
+              @click="showDescription = !showDescription"
+              :color="showDescription ? 'yellow.200' : null"
+              :transform="showDescription ? 'scale(1.3)' : null"
+              :_hover="{ transform: 'scale(1.3)' }"
+              ariaLabel="show-description"
+            />
+          </CFlex>
+          <CCollapse :isOpen="showDescription" v-if="removeFirstShow">
+            <CText px="1rem" pb="1rem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              maiores quidem praesentium sapiente quasi odit at fugiat, labore
+              quos optio voluptates ipsum commodi eos dolor quis autem quam ut
+              neque?
+            </CText>
+          </CCollapse>
+        </CBox>
+      </CFlex>
+    </CBox>
   </CBox>
 </template>
 
@@ -87,11 +99,13 @@ import {
   CIconButton,
   CHeading,
   CText,
+  CCollapse,
 } from "@chakra-ui/vue";
 
 export default {
   name: "CardProfile",
   components: {
+    CCollapse,
     CHeading,
     CText,
     CFlex,
@@ -103,12 +117,62 @@ export default {
 
   data() {
     return {
-      startDragPos: null,
+      showDescription: false,
+      photos: [0, 1, 2, 3],
+      showPhotoNumber: 0,
+      removeFirstShow: false,
     };
   },
 
-  methods: {},
+  methods: {
+    prevPhoto() {
+      if (this.showPhotoNumber > 0) {
+        console.log("low");
+        this.showPhotoNumber--;
+      }
+    },
+
+    nextPhoto() {
+      if (this.showPhotoNumber < 3) {
+        this.showPhotoNumber++;
+      }
+    },
+  },
+
+  watch: {
+    showDescription() {
+      if (!this.removeFirstShow) {
+        this.removeFirstShow = true;
+      }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.animationShow {
+  animation: show 0.05s linear;
+}
+
+.animationNotShow {
+  animation: notShow 0.05s linear;
+}
+
+@keyframes show {
+  from {
+    height: 3px;
+  }
+  to {
+    height: 6px;
+  }
+}
+
+@keyframes notShow {
+  from {
+    height: 6px;
+  }
+  to {
+    height: 3px;
+  }
+}
+</style>
