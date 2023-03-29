ca152b989ff7e023b9efed1243bd73838347816a
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2023-03-29 20:35:07 +0200

    #46 push profile object to firebase (#47)
    * install geofire package
    * methods for add profile and get by range
    * rework for new function
    * rework of form object
    * import fix

---
+++ src/firebase/profileFirebase.js
@@ -0,0 +1,54 @@
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  startAt,
+  endAt,
+} from "firebase/firestore";
+
+import { geohashQueryBounds, distanceBetween } from "geofire-common";
+
+import db from "@/firebase";
+
+const colRef = collection(db, "profile");
+
+export async function addProfile(profile) {
+  const docRef = await addDoc(colRef, profile);
+}
+
+export async function getProfilesByRange(point, range) {
+  const radiusInM = range * 1000;
+
+  const bounds = geohashQueryBounds(point, radiusInM);
+  const promises = [];
+
+  bounds.forEach((b) => {
+    const q = query(
+      colRef,
+      orderBy("city.position.geohash"),
+      startAt(b[0]),
+      endAt(b[1])
+    );
+    promises.push(getDocs(q));
+  });
+
+  Promise.all(promises)
+    .then((snapshots) => {
+      const matchingDocs = [];
+      snapshots.map((snap) => {
+        snap.forEach((doc) => {
+          const docPoint = doc.data().city?.position?.coordinates;
+          const distanceInM = distanceBetween(docPoint, point) * 1000;
+          if (distanceInM <= radiusInM) matchingDocs.push(doc.data());
+        });
+      });
+      return matchingDocs;
+    })
+    .then((matchingDocs) => {
+      // do something with matched profile
+      // profile mached only by range
+      console.log(matchingDocs);
+    });
+}
